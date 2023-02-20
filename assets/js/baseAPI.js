7cb3200f81be62fcb046e0d71ec998d05447839d
RikkaMei <707871728@qq.com>
2023-02-20 10:13:01 +0800

    完成了登录的修改

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,8 @@
+// $.ajaxPrefilter() 可以在请求之前修改默认请求参数，这里的作用是自动拼接根路径
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
-// 会先调用 ajaxPrefilter 这个函数
-// 在这个函数中，可以拿到我们给Ajax提供的配置对象
+// 会先调用 ajaxPrefilter 这个函数,可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+  options.url = 'http://www.liulongbin.top:3007' + options.url
+  console.log(options.url);
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,29 +1,32 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
+$(function () {
+
+  // 切换到注册界面
+  $('#link_reg').on('click', () => {
     $('.login-box').hide()
     $('.reg-box').show()
   })
 
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
-    $('.login-box').show()
+  // 切换到登陆界面
+  $('#link_login').on('click', () => {
     $('.reg-box').hide()
+    $('.login-box').show()
   })
 
-  // 从 layui 中获取 form 对象
+  // 【表单验证规则】
+
+  // 从layui获取form对象
   var form = layui.form
+  // 设置弹出层，用于弹出提示信息
   var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
+
+  // 自定义表单验证规则
   form.verify({
-    // 自定义了一个叫做 pwd 校验规则
+    // 密码格式
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
+    // 确认密码判断
+    repwd: function (value) {
+      // value为当前表单中输入的内容
+      // 需获取第一次密码的值，来与第二次密码进行比较 val()返回指定的值
       var pwd = $('.reg-box [name=password]').val()
       if (pwd !== value) {
         return '两次密码不一致！'
@@ -31,44 +34,51 @@ $(function() {
     }
   })
 
-  // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
-    e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
-    }
-    $.post('/api/reguser', data, function(res) {
-      if (res.status !== 0) {
-        return layer.msg(res.message)
-      }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
-      $('#link_login').click()
-    })
-  })
-
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
+  // 【监听注册表单的提交事件】
+  $('#form_reg').on('submit', function (e) {
     // 阻止默认提交行为
     e.preventDefault()
+    // 发起ajax的POST请求
+
+    // 获取表单中输入的账号密码
+    var data = { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() }
+    // 因为使用了自定义拼接根路径的脚本，所以url可以简化
+    $.post(
+      '/api/reguser', data, function (res) {
+        // 如果请求失败，返回提示信息
+        if (res.status !== 0) {
+          return layer.msg(res.message)
+        }
+        layer.msg('注册成功')
+        // 注册完成后自动切换到登陆界面
+        $('#link_login').click()
+      }
+    )
+  })
+
+  // 【监听登录表单的提交事件】
+  $('#form_login').on('submit', function (e) {
+    // 阻止默认提交行为
+    e.preventDefault()
+    // 发起ajax请求
     $.ajax({
       url: '/api/login',
       method: 'POST',
-      // 快速获取表单中的数据
+      // serialize()序列化表单元素，用于快速获取表单元素
       data: $(this).serialize(),
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
-          return layer.msg('登录失败！')
+          // console.log(res.message);
+          return layer.msg('登陆失败！')
         }
-        layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
+        layer.msg('登陆成功')
+        console.log(res.token);
+        // 登陆成功后获得的 token 保存到localStorage中
         localStorage.setItem('token', res.token)
-        // 跳转到后台主页
+        // 最后跳转到后台主页 （location.href可以进行网页重定向等功能
         location.href = '/index.html'
       }
     })
   })
+
 })
